e55d468d77f070762b35d8eda54a9faf931dd798
Scott MacLachlan <scott.maclachlan1@gmail.com>
2025-09-14 22:13:33 +0100

    Implement task display in App component with updated styles

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Tasks from './componets/tasks';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          <Tasks text='This is a task 1' />
+          <Tasks text='This is a task 2' />
+          <Tasks text='This is a task 3' />
+        </View>
+      </View> 
     </View>
   );
 }
@@ -13,8 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 });
